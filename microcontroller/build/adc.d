cc4497e4858e39b6459f29ffe07c93c3ef7e8c83
BalderHolst <balderwh@gmail.com>
2024-03-19 14:11:06 +0100

    microcontroller: ignore build directory

--- microcontroller/build/adc.d
+++
@@ -1,9 +0,0 @@
-build/adc.o: adc.c \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/stdint.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/machine/_default_types.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/sys/features.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/_newlib_version.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/sys/_intsup.h \
- /nix/store/i8p069hg9yhdqp764gaqs22wga33xnyl-gcc-arm-embedded-9.3.1/arm-none-eabi/include/sys/_stdint.h \
- tm4c123gh6pm.h emp_type.h
--- microcontroller/build/uart.d
+++
@@ -1,1 +0,0 @@
-build/uart.o: uart.c uart0.h emp_type.h
