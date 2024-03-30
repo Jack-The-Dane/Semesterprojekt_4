3e27c4d65a10e1ab543073baa68c5f8cbe61d847
BalderHolst <balderwh@gmail.com>
2024-03-30 14:00:27 +0100

    microcontroller: read joystick button

--- microcontroller/joystick.h
+++ microcontroller/joystick.h
@@ -1,8 +1,11 @@
 #include "adc.h"
 #include "emp_type.h"
+#include "tm4c123gh6pm.h"
 
 #ifndef JOYSTICK
 #define JOYSTICK
+
+#define JOYSTICK_BUTTON_PIN 0
 
 struct Joystick {
     INT16U x, y;
@@ -11,12 +14,19 @@ struct Joystick {
 
 void init_joystick() {
     init_adc();
+
+    // Setup PB0 as input for the joystick button
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB;            // Enable the GPIO port
+    GPIO_PORTB_DIR_R &= ~(1 << JOYSTICK_BUTTON_PIN); // Set the direction as output
+    GPIO_PORTB_DEN_R |=  (1 << JOYSTICK_BUTTON_PIN); // Enable the GPIO pin
+    GPIO_PORTB_PUR_R |=  (1 << JOYSTICK_BUTTON_PIN); // Enable internal pull-up
+
 }
 
 void update_joystick(struct Joystick * stick) {
     stick->x = get_adc0();
     stick->y = get_adc1();
-    stick->button = 0;
+    stick->button = !(GPIO_PORTB_DATA_R & (1 << JOYSTICK_BUTTON_PIN));
 }
 
 #endif
--- microcontroller/main.c
+++ microcontroller/main.c
@@ -1,14 +1,15 @@
-#include "emp_type.h"
 #include "gpio.h"
 #include "spi.h"
 #include "uart.h"
 #include "joystick.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#include "tm4c123gh6pm.h"
 #include <stdint.h>
+
+#include "emp_type.h"
+#include "tm4c123gh6pm.h"
 
 void setup() {
     setupPortF();
@@ -29,7 +30,21 @@ int main(void) {
     struct Joystick stick = {0};
 
     while (69) {
-        /* update_joystick(&stick); */
+        update_joystick(&stick);
+
+        uint8_t x_low = stick.x;
+        uint8_t x_high = stick.x >> 8;
+        uint8_t y_low = stick.y;
+        uint8_t y_high = stick.y >> 8;
+
+        uart_putc(x_high);
+        uart_putc(x_low);
+        uart_putc(y_high);
+        uart_putc(y_low);
+
+        while (stick.button) {
+            update_joystick(&stick);
+        }
 
     }
 
