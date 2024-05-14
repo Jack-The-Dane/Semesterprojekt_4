64077b77edb303372f7021ba4fe230f8ea11eb2e
BalderHolst <balderwh@gmail.com>
2024-05-14 08:32:47 +0200

    microcontroller: new uart

--- microcontroller/src/uart0.c
+++ microcontroller/src/uart0.c
@@ -1,204 +1,57 @@
-/*****************************************************************************
-* University of Southern Denmark
-* Embedded Programming (EMP)
-*
-* MODULENAME.: emp.c
-*
-* PROJECT....: EMP
-*
-* DESCRIPTION: See module specification file (.h-file).
-*
-* Change Log:
-*****************************************************************************
-* Date    Id    Change
-* YYMMDD
-* --------------------
-* 150228  MoH   Module created.
-*
-*****************************************************************************/
-
-/***************************** Include files *******************************/
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 #include "emp_type.h"
-/*****************************    Defines    *******************************/
 
-/*****************************   Constants   *******************************/
+void vSetupUart0() {
+  // Enable the UART0 clock
+  SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
+  // Set the alternate function for PA0 and PA1
+  GPIO_PORTA_AFSEL_R |= 0b00000011;
+  // Enable the alternate function for PA0 and PA1
+  GPIO_PORTA_PCTL_R |= 0x00000011;
+  // Enable the GPIO pins for digital function (PA0 and PA1)
+  GPIO_PORTA_DEN_R |= 0b00000011;
+  // Handle direction of the pins
+  GPIO_PORTA_DIR_R |= 0b00000010;
 
-/*****************************   Variables   *******************************/
+  // Disable UART0
+  UART0_CTL_R &= ~(UART_CTL_UARTEN);
 
+  INT32U baud_rate = 115200;
+  INT32U brd;
 
-/*****************************   Functions   *******************************/
+  // Set the baud rate
+  // IBRD = int(16,000,000 / (16 * 9600)) = int(104.1667)
+  // Set the fractional part of the baud rate
+  // FBRD = 0.1667 * 64 + 0.5 = int(11,16688)
+  brd = 64000000 / baud_rate; // X-sys*64/(16*baudrate) = 16M*4/baudrate
+  UART0_IBRD_R = brd / 64;
+  UART0_FBRD_R = brd & 0x0000003F;
 
+  // Set the transmission parameters
+  UART0_LCRH_R = 0b01100000;  // 8-bit word length
+  UART0_LCRH_R |= 0b00000110; // even parity
+  UART0_LCRH_R |= (1 << 4);   // Enable FIFO
 
-INT32U lcrh_databits( INT8U antal_databits )
-/*****************************************************************************
-*   Input    :
-*   Output   :
-*   Function : sets bit 5 and 6 according to the wanted number of data bits.
-*   		    5: bit5 = 0, bit6 = 0.
-*   		    6: bit5 = 1, bit6 = 0.
-*   		    7: bit5 = 0, bit6 = 1.
-*   		    8: bit5 = 1, bit6 = 1  (default).
-*   		   all other bits are returned = 0
-******************************************************************************/
-{
-  if(( antal_databits < 5 ) || ( antal_databits > 8 ))
-	antal_databits = 8;
-  return(( (INT32U)antal_databits - 5 ) << 5 );  // Control bit 5-6, WLEN
+  // Set the clock source
+  UART0_CC_R = 0;
+  // Enable the UART0
+  UART0_CTL_R |=
+      (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN); // Enable UART0
 }
 
-INT32U lcrh_stopbits( INT8U antal_stopbits )
-/*****************************************************************************
-*   Input    :
-*   Output   :
-*   Function : sets bit 3 according to the wanted number of stop bits.
-*   		    1 stpobit:  bit3 = 0 (default).
-*   		    2 stopbits: bit3 = 1.
-*   		   all other bits are returned = 0
-******************************************************************************/
-{
-  if( antal_stopbits == 2 )
-    return( 0x00000008 );  		// return bit 3 = 1
-  else
-	return( 0x00000000 );		// return all zeros
-}
-
-INT32U lcrh_parity( INT8U parity )
-/*****************************************************************************
-*   Input    :
-*   Output   :
-*   Function : sets bit 1, 2 and 7 to the wanted parity.
-*   		    'e':  00000110b.
-*   		    'o':  00000010b.
-*   		    '0':  10000110b.
-*   		    '1':  10000010b.
-*   		    'n':  00000000b.
-*   		   all other bits are returned = 0
-******************************************************************************/
-{
-  INT32U result;
-
-  switch( parity )
-  {
-    case 'e':
-      result = 0x00000006;
-      break;
-    case 'o':
-      result = 0x00000002;
-      break;
-    case '0':
-      result = 0x00000086;
-      break;
-    case '1':
-      result = 0x00000082;
-      break;
-    case 'n':
-    default:
-      result = 0x00000000;
+void vUart0_task(void *pvParameters) {
+  // Task
+  INT8U uart0_data;
+  while (1) {
+    while (xQueueReceive(q_uartTx, &uart0_data, 0)) {
+      send_char(uart0_data);
+    }
+    while (!(UART0_FR_R & (1 << 4))) {
+      uart0_data = UART0_DR_R;
+      xQueueSendToBack(q_uartRx, &uart0_data, 1);
+    }
+    vTaskDelay(50 / portTICK_RATE_MS); // wait 500 ms.
   }
-  return( result );
+  vTaskDelete(NULL);
 }
-
-void uart0_fifos_enable()
-/*****************************************************************************
-*   Input    :
-*   Output   :
-*   Function : Enable the tx and rx fifos
-******************************************************************************/
-{
-  UART0_LCRH_R  |= 0x00000010;
-}
-
-void uart0_fifos_disable()
-/*****************************************************************************
-*   Input    :
-*   Output   :
-*   Function : Enable the tx and rx fifos
-******************************************************************************/
-{
-  UART0_LCRH_R  &= 0xFFFFFFEF;
-}
-
-
-extern BOOLEAN uart0_rx_rdy()
-/*****************************************************************************
-*   Function : See module specification (.h-file).
-*****************************************************************************/
-{
-  return( UART0_FR_R & UART_FR_RXFF );
-}
-
-extern INT8U uart0_getc()
-/*****************************************************************************
-*   Function : See module specification (.h-file).
-*****************************************************************************/
-{
-  return ( UART0_DR_R );
-}
-
-extern BOOLEAN uart0_tx_rdy()
-/*****************************************************************************
-*   Function : See module specification (.h-file).
-*****************************************************************************/
-{
-  return( UART0_FR_R & UART_FR_TXFE );
-}
-
-extern void uart0_putc( INT8U ch )
-/*****************************************************************************
-*   Function : See module specification (.h-file).
-*****************************************************************************/
-{
-  UART0_DR_R = ch;
-}
-
-extern void uart0_init( INT32U baud_rate, INT8U databits, INT8U stopbits, INT8U parity )
-/*****************************************************************************
-*   Function : See module specification (.h-file).
-*****************************************************************************/
-{
-  INT32U BRD;
-
-  #ifndef E_PORTA
-  #define E_PORTA
-  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA;					// Enable clock for Port A
-  #endif
-
-  #ifndef E_UART0
-  #define E_UART0
-  SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0;					// Enable clock for UART 0
-  #endif
-
-  GPIO_PORTA_AFSEL_R |= 0x00000003;		// set PA0 og PA1 to alternativ function (uart0)
-  GPIO_PORTA_DIR_R   |= 0x00000002;     // set PA1 (uart0 tx) to output
-  GPIO_PORTA_DIR_R   &= 0xFFFFFFFE;     // set PA0 (uart0 rx) to input
-  GPIO_PORTA_DEN_R   |= 0x00000003;		// enable digital operation of PA0 and PA1
-  //GPIO_PORTA_PUR_R   |= 0x00000002;
-
-  BRD = 64000000 / baud_rate;   	// X-sys*64/(16*baudrate) = 16M*4/baudrate
-  UART0_IBRD_R = BRD / 64;
-  UART0_FBRD_R = BRD & 0x0000003F;
-
-  UART0_LCRH_R  = lcrh_databits( databits );
-  UART0_LCRH_R += lcrh_stopbits( stopbits );
-  UART0_LCRH_R += lcrh_parity( parity );
-
-  uart0_fifos_disable();
-
-  UART0_CTL_R  |= (UART_CTL_UARTEN | UART_CTL_TXE );  // Enable UART
-}
-
-/****************************** End Of Module *******************************/
-
-
-
-
-
-
-
-
-
-
-
-
