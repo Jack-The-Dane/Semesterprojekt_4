d0dc3bcc2f2a33f25fbc6f1208b73d43f5b74f74
BalderHolst <balderwh@gmail.com>
2024-05-23 13:50:54 +0200

    controller: c implemetation of controller

--- c-matrix/main.c
+++ c-matrix/main.c
@@ -3,34 +3,79 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void step_controller(double ref[2][1], double x[4][1], double y[2][1], double time_step) {
-    static double control_integrator[2][1] = {0};
+void step_controller(double ref[][1], double y[][1], void * u, double time_step) {
+
+    // Observer states
+    static double x_hat[4][1]     = {0};
+    static double x_hat_dot[4][1] = {0};
+
+    // Integrators
+    static double control_integrator[2][1]  = {0};
     static double observer_integrator[4][1] = {0};
 
     // Update control integrator
-    double neg_ref[2][1];
-    printf("hellO");
-    matrix_scale(neg_ref, -1);
-    matrix_add(control_integrator, neg_ref);
+    matrix_subtract(control_integrator, ref);
     matrix_add(control_integrator, y);
 
     // Calculate the new input
-    double u[2][1];
-    double res[2][1];
-    matrix_multiply(FI, control_integrator, res);
+    double u_temp[2][1] = {0};
+    double u_in1[2][1];
+    double u_in2[2][1];
+    matrix_multiply(FI, control_integrator, u_in1);
+    matrix_multiply(F, x_hat, u_in2);
+    matrix_add(u_temp, u_in1);
+    matrix_add(u_temp, u_in2);
 
+    // Calculate observer output
+    double y_hat[2][1];
+    matrix_multiply(C, x_hat, y_hat);
+
+    // Update observer integrator
+    {
+        double obs_in1[4][1];
+        double obs_in2[4][1];
+        double obs_in3[4][1];
+        matrix_multiply(A, x_hat, obs_in1);
+        matrix_multiply(B, u_temp,    obs_in2);
+
+        // Calculate observer output error
+        double L_in[2][1] = {0};
+        matrix_add(L_in, y_hat);
+        matrix_subtract(L_in, y);
+        matrix_multiply(L, L_in, obs_in3);
+
+        matrix_add(observer_integrator, obs_in1);
+        matrix_add(observer_integrator, obs_in2);
+        matrix_add(observer_integrator, obs_in3);
+    }
+
+    memcpy(u, u_temp, sizeof(double)*2);
 }
 
 int main(void)
 {
-    double x[4][1] = {0, 0, 0, 0};
-    double ref[2][1] = {0, 0};
-    double y[2][1] = {0, 0};
-    double time_step = 0.01;
-    step_controller(ref, x, y, time_step);
+    double ref[2][1] = {1, 1};
+    double y[2][1] = {0};
+    double u[2][1] = {0};
+    double time_step = 0.5;
 
-    matrix_print(B);
+    for (int i = 0; i < 10; i++) {
+        step_controller(ref, y, &u, time_step);
+        matrix_print(u);
+    }
+
+    y[0][0] = 1;
+    y[0][1] = 1;
+
+    printf("\n\n");
+
+    for (int i = 0; i < 10; i++) {
+        step_controller(ref, y, &u, time_step);
+        matrix_print(u);
+    }
+
 
     return 0;
 
