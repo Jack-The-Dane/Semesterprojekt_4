44ccaea7c56e216c209b46c78fe5bb559bce9168
simone-ingvild <simone.lebech@gmail.com>
2024-05-24 14:01:41 +0200

    semaphore added

--- CCS_workspace/Microcontroller_CCS/src/main.c
+++ CCS_workspace/Microcontroller_CCS/src/main.c
@@ -20,6 +20,7 @@
 
 volatile SemaphoreHandle_t joystick_mutex;
 volatile SemaphoreHandle_t joystick_uart_mutex;
+volatile SemaphoreHandle_t encoder_mutex;
 
 volatile xQueueHandle q_uart_tx;
 volatile xQueueHandle q_uart_rx;
@@ -44,9 +45,11 @@ int main(void) {
 
     joystick_mutex = xSemaphoreCreateMutex();
     joystick_uart_mutex = xSemaphoreCreateMutex();
+    encoder_mutex = xSemaphoreCreateMutex();
 
     if (!joystick_mutex) error();
     if (!joystick_uart_mutex) error();
+    if (!encoder_mutex) error();
 
     q_uart_tx = xQueueCreate(150, sizeof(INT8U));
     q_uart_rx = xQueueCreate(150, sizeof(INT8U));
