a78ab6b48889927a233e6d8585a33d6f60761eff
TheJoboReal <kasper.jobo@gmail.com>
2024-05-27 16:09:50 +0200

    Microcontroller: Added comments to vel_measure function

--- CCS_workspace/Microcontroller_CCS/src/controller.c
+++ CCS_workspace/Microcontroller_CCS/src/controller.c
@@ -14,15 +14,16 @@
 
 
 // How much time is allocated for the other tasks running on the microcontroller (5 ms / tick)
-#define CONTROLLER_EXTRA_SLEEP_TICKS 5
-#define VEL_SIZE 10      // Size of the velocity buffer
-#define PI 3.14159265358979323846
-#define TICKS_PR_REV 285
-#define TICKS_TO_RAD = 0.02204626423
+#define CONTROLLER_EXTRA_SLEEP_TICKS 5      
+#define VEL_SIZE 10                         // Size of the velocity buffer
+#define PI 3.14159265358979323846           // PI aka 3
+#define TICKS_PR_REV 285                    // Number of ticks per revolution
+#define TICKS_TO_RAD 0.02204626423        // 2 * PI / 285
 
 
 
 
+const TickType_t xFrequency = pdMS_TO_TICKS( (SCLK_HALF_PERIOD_US * (SPI_WORD_LENGTH * 2 + 2)) / 1000 ) + CONTROLLER_EXTRA_SLEEP_TICKS;
 double v_tilt[VEL_SIZE];
 double v_pan[VEL_SIZE];
 double u_temp[2][1];
@@ -35,7 +36,7 @@ extern TaskHandle_t serial_task_handle;
 
 double tics_to_rad(INT16S ticks){
     ticks = ticks % TICKS_PR_REV;
-    return ((double) ticks) * 0.02204626423;
+    return ((double) ticks) * TICKS_TO_RAD;
 };
 
 double dist(double p1, double p2){
@@ -55,28 +56,23 @@ void send_debug_value(SPI_TYPE encoders){
     xQueueSendToBack(q_uart_tx, &byte3, 0);
     xQueueSendToBack(q_uart_tx, &byte4, 0);
 }
-const TickType_t xFrequency = pdMS_TO_TICKS( (SCLK_HALF_PERIOD_US * (SPI_WORD_LENGTH * 2 + 2)) / 1000 ) + CONTROLLER_EXTRA_SLEEP_TICKS;
 
 
-
-
-// Velocity measurer
 void vel_measurer(){
     static INT8U i = 0;
-    static double theta_last_tilt = 0;
-    static double theta_last_pan = 0;
-    static double theta_current_tilt = 0;
-    static double theta_current_pan = 0;
+    static double theta_last_tilt = 0;      // Tilt position from last iteration
+    static double theta_last_pan = 0;       // Pan position from last iteration
+    static double theta_current_tilt = 0;   // Tilt position from current iteration
+    static double theta_current_pan = 0;    // Pan position from current iteration
 
     if(i % VEL_SIZE == 0){i = 0;}
-    theta_current_pan = tics_to_rad((encoders >> 2) & 0x1FF);
-    theta_current_tilt = tics_to_rad((encoders >> 11) & 0x1FF);
+    theta_current_pan = tics_to_rad((encoders >> 2) & 0x1FF);       // Get pan position
+    theta_current_tilt = tics_to_rad((encoders >> 11) & 0x1FF);     // Get tilt position
 
 
     // Calulate pan velocity and place in array
     v_pan[i] = ((dist(theta_last_pan, theta_current_pan)) * 1000) / (xFrequency * portTICK_PERIOD_MS);
     v_tilt[i] = ((dist(theta_last_tilt, theta_current_tilt)) * 1000) / (xFrequency * portTICK_PERIOD_MS);
-    send_char((char) (v_tilt[i] * 100));
 
     // Sum of velocities
     double vel_sum_pan = 0;
@@ -89,7 +85,7 @@ void vel_measurer(){
     u_temp[0][0] = vel_sum_pan / VEL_SIZE;
     u_temp[1][0] = vel_sum_tilt / VEL_SIZE;     // Get average of tilt velocities
 
-    send_char((char) (u_temp[1][0] * 100));
+    send_char((char) (u_temp[1][0] * 100));     // Send tilt velocity over uart
 
     // Iterate and set used positions as old
     i++;
--- CCS_workspace/Microcontroller_CCS/src/inc/controller.h
+++ CCS_workspace/Microcontroller_CCS/src/inc/controller.h
@@ -6,7 +6,15 @@
 
 SPI_TYPE encoders;
 
+// Function to calculate the angular velocity of pan and tilt
 void vel_measurer();
+
+// Function to calculate the distance between two points. Returns the distance as a double.
+double dist(double p1, double p2);
+
+// Function to convert encoder ticks to radians. Returns the angle in radians as a double.
+double tics_to_rad(INT16S ticks);
+
 void controller_task();
 
 #endif
